85a2e8fe42472c6063876da17d569ae593d253ae
ammoru <22a91a61g2@aec.edu.in>
2025-02-21 06:32:32 +0530

    reslove scrollup

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter } from "next/font/google";
 
 import "./globals.css";
 import { ThemeProvider } from "./provider";
-import Scrollup from "./scrollup";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
